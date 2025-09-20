dfa4a4dca9481c28e543c291e39f2e32fe691659
jane <jayndiritu36@gmail.com>
2025-09-20 20:41:45 +0300

    Simple Calculator with Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,4 +1,4 @@
-num1 = input("Enter the first number")
+num1 = input("Enter the first number: ")
 num2 = input("Enter the second number")
 num1, num2 = map(float, (num1, num2))
 operation = input("Choose an operation (add, subtract, multiply, divide): ")
