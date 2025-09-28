74db8a36d35a8471a90c288fa00234a7685d5725
jane <jayndiritu36@gmail.com>
2025-09-28 11:57:37 +0300

    Temperature Conversion Tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,19 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+def  convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+temp = input("Enter temperature (e.g., 100F or 37C): ").strip().upper()
+if temp.endswith("F"):
+    fahrenheit = float(temp[:-1])
+    celsius = convert_to_celsius(fahrenheit)
+    print(f"{fahrenheit}F is {celsius:.2f}C")
+elif temp.endswith("C"):
+    celsius = float(temp[:-1])
+    fahrenheit = convert_to_fahrenheit(celsius)
+    print(f"{celsius}C is {fahrenheit:.2f}F")
+else:
+    print("Invalid input format.")
