75e5def3d2a930856bd7b6c491d66e6854541951
jane <jayndiritu36@gmail.com>
2025-10-02 12:40:20 +0300

    a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self,  str, initial_balance: float = 0.0):
+    def __init__(self, initial_balance: float = 0.0):
         self.balance = initial_balance
 
     def deposit(self, amount: float) -> None:
@@ -11,8 +11,9 @@ class BankAccount:
         if amount <= 0:
             raise ValueError("Withdrawal amount must be positive.")
         if amount > self.balance:
-            raise ValueError("Insufficient funds.")
+          return False
         self.balance -= amount
+        return True
 
     def display_balance(self) -> None:
         print(f"Current Balance: {self.balance:.2f}")
